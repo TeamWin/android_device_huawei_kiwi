e674f25ea8f4a7eef3d8eb047c3c2968cc44df4e
Paul B <plblaze@blazebox.homeip.net>
2017-04-18 20:05:01 -0400

    kiwi: restore TARGET_USE_CUSTOM_LUN_FILE_PATH option
    Inadvertently removed option, this commit restores it.
    
    Change-Id: Ic5960889bb59c4420bf009e13e29170c805a229b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,3 +86,7 @@ TW_NO_EXFAT_FUSE := true
 TW_INCLUDE_NTFS_3G := true
 TW_INCLUDE_CRYPTO := true
 TARGET_HW_DISK_ENCRYPTION := true
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
